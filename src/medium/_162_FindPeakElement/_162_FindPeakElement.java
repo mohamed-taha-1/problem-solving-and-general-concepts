a7ff86325fbb7c6b34c404a6bd46629b9791f2e5
mohamed-taha-1 <mohamedtahaabdel@gmail.com>
2023-08-25 17:44:36 +0200

    new medium peek-elment

---
+++ src/medium/_162_FindPeakElement/_162_FindPeakElement.java
@@ -0,0 +1,13 @@
+package medium._162_FindPeakElement;
+
+public class _162_FindPeakElement {
+	
+	
+	// A peak element is an element that is strictly greater than its neighbors.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
